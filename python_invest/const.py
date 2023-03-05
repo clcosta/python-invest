7ec4fc5850137dad160a1e9ce006f3d2964e70d0
Claudio Lima <clcostaf@hotmail.com>
2023-03-05 00:42:29 -0300

    feat: Using firebase firestore to get list of cryptos + tests

--- python_invest/const.py
+++ python_invest/const.py
@@ -6,3 +6,7 @@ PRODUCT_LIST = ['cryptos', 'stocks', 'funds']
 
 # Time Frame
 TIME_FRAME_LIST = ['daily', 'monthly', 'weekly']
+
+# Firestore API
+FB_BASE_URL = 'https://firestore.googleapis.com/v1/projects/python-invest-67b1c/databases/(default)/documents/'
+FB_CRYPTO_DOC_ID = 'crypto/IEVxWEmy32J3FDlvq9Cj'
